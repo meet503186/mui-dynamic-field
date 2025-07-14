40366ed3e7223ca81cf085b8f57519037a828c6e
Harmeet Singh <harmeet@msgitwingtech.com>
2025-07-14 14:28:48 +0530

    removed mui-file-uploader from external rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,6 @@ export default defineConfig({
         "@mui/icons-material",
         "@emotion/react",
         "@emotion/styled",
-        "mui-file-uploader",
       ],
       output: {
         globals: {
