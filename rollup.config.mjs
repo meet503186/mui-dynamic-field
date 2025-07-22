2fe4b665dceaf9be8f6bf843ec389fb11a727c2b
Harmeet Singh <harmeet@msgitwingtech.com>
2025-07-22 18:52:16 +0530

    refactor: migrate from Vite to Rollup for build process
    - Updated build script in package.json to use Rollup instead of Vite.
    - Added rollup.config.mjs for Rollup configuration, including TypeScript and Babel support.
    - Removed vite.config.ts as it is no longer needed.
    - Updated peerDependencies and devDependencies in package.json to include necessary Rollup plugins.
    - Added tslib as a dependency for better TypeScript support.

---
+++ rollup.config.mjs
@@ -0,0 +1,57 @@
+// rollup.config.mjs
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+
+const extensions = [".ts", ".tsx"];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/mui-dynamic-field.js",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/mui-dynamic-field.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/mui-dynamic-field.min.js",
+        format: "esm",
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.node.json",
+        jsx: "preserve",
+      }),
+      babel({
+        babelHelpers: "bundled",
+        extensions,
+        exclude: "node_modules/**",
+        presets: [
+          ["@babel/preset-react", { runtime: "automatic" }],
+          "@babel/preset-typescript",
+        ],
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/types/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
